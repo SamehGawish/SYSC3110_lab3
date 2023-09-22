1aac3f6d78d90919caed7d184f5dc3c9cd1d8d91
Sameh Gawish <samehgawish@gmail.com>
2023-09-22 15:09:02 -0400

    this is the second commit using IntelliJ for lab2 in SYSC3110

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -20,6 +20,11 @@ public class AddressBook {
 
     public static void main(String[] args) {
         System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo("Faris");
+        AddressBook addressBook = new AddressBook();
+
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
 
     }
 
